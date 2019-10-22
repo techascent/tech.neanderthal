d400f97bbf694f300840eff28658171d78927fc0
Chris Nuernberger <chris@techascent.com>
2019-10-22 07:28:46 -0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# tech.neanderthal
+
+Neandtheral bindings to the techascent ecosystem.
+
+
+
+## License
+
+Copyright © 2019 Techascent, LLC
+
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject tech.neanderthal "0.1.0-SNAPSHOT"
+  :description "Neanderthal bindings to the techascent ecosystem"
+  :url "http://github.com/techascent/tech.neanderthal"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [uncomplicate/neanderthal "0.26.0"]]
+  )

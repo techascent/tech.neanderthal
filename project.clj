184964d455d6b440e0f48f82534e1e5cb5fd6be1
Chris Nuernberger <chris@techascent.com>
2019-10-22 09:52:46 -0600

    Basic zero copy access is working.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject tech.neanderthal "0.1.0-SNAPSHOT"
+(defproject tech.neanderthal "4.26-1-SNAPSHOT"
   :description "Neanderthal bindings to the techascent ecosystem"
   :url "http://github.com/techascent/tech.neanderthal"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [uncomplicate/neanderthal "0.26.0"]]
-  )
+                 [uncomplicate/neanderthal "0.26.0"]
+                 [techascent/tech.datatype "4.26"]])

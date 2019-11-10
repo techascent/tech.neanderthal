cf4845623f2ad03946727d436b6452f4756d221a
Chris Nuernberger <chris@techascent.com>
2019-11-10 14:23:17 -0700

    4.44

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject techascent/tech.neanderthal "4.26-1-SNAPSHOT"
+(defproject techascent/tech.neanderthal "4.44"
   :description "Neanderthal bindings to the techascent ecosystem"
   :url "http://github.com/techascent/tech.neanderthal"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [uncomplicate/neanderthal "0.26.0"]
-                 [techascent/tech.datatype "4.26"]])
+                 [techascent/tech.datatype "4.44"]])
